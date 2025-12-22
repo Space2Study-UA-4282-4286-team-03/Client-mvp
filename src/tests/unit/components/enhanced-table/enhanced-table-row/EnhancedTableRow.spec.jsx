33c7c522ce4360a2bf83c43b40b0c543ca83dfb0
wareyuu <ale167282@gmail.com>
2025-12-22 23:18:15 +0200

    tests for enhanced table row components

---
+++ src/tests/unit/components/enhanced-table/enhanced-table-row/EnhancedTableRow.spec.jsx
@@ -0,0 +1,95 @@
+import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import EnhancedTableRow from '~/components/enhanced-table/enhanced-table-row/EnhancedTableRow'
+
+vi.mock('react-i18next', () => ({
+  useTranslation: () => ({ t: (key) => key })
+}))
+
+vi.mock('react-router-dom', () => ({
+  useNavigate: vi.fn()
+}))
+
+const openMenuMock = vi.fn()
+const closeMenuMock = vi.fn()
+
+vi.mock('~/hooks/use-menu', () => ({
+  default: () => ({
+    openMenu: openMenuMock,
+    closeMenu: closeMenuMock,
+    renderMenu: (items) => <div>{items}</div>
+  })
+}))
+
+describe('EnhancedTableRow component', () => {
+  const handleSelectClickMock = vi.fn()
+  const isSelectedMock = vi.fn(() => false)
+  const onActionMock = vi.fn()
+
+  beforeEach(() => {
+    const item = {
+      _id: '123',
+      name: 'TestName'
+    }
+
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow
+            columns={[{ field: 'name', label: 'Name' }]}
+            isSelection
+            item={item}
+            onRowClick={() => {}}
+            refetchData={vi.fn()}
+            rowActions={[{ label: 'Delete', func: onActionMock }]}
+            select={{
+              isSelected: isSelectedMock,
+              handleSelectClick: handleSelectClickMock
+            }}
+            selectedRows={[item]}
+          />
+        </tbody>
+      </table>
+    )
+  })
+
+  it('render table row with correct data', () => {
+    expect(screen.getByText('TestName')).toBeInTheDocument()
+  })
+
+  it('call handleSelectClick when checkbox is clicked', async () => {
+    const user = userEvent.setup()
+    const checkbox = screen.getByRole('checkbox')
+
+    await user.click(checkbox)
+
+    expect(handleSelectClickMock).toHaveBeenCalledTimes(1)
+  })
+
+  it('render action menu when menu icon is clicked', async () => {
+    const user = userEvent.setup()
+    const menuIcon = screen.getByTestId('menu-icon')
+
+    await user.click(menuIcon)
+
+    expect(screen.getByText('Delete')).toBeInTheDocument()
+  })
+
+  it('call onAction function when clicking on the menu item', async () => {
+    const user = userEvent.setup()
+
+    const actionButton = screen.getByText('Delete')
+    await user.click(actionButton)
+
+    expect(onActionMock).toHaveBeenCalledWith('123')
+  })
+
+  it('close menu when "escape" is pressed', async () => {
+    const user = userEvent.setup()
+
+    await user.keyboard('{Escape}')
+
+    expect(closeMenuMock).toHaveBeenCalledTimes(1)
+  })
+})
