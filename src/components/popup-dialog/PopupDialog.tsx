d04da1ef7fcd7a657fd6ba849efac05ad501d941
wareyuu <ale167282@gmail.com>
2025-12-26 16:23:59 +0200

    added confirm popup

--- src/components/popup-dialog/PopupDialog.tsx
+++ src/components/popup-dialog/PopupDialog.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Box from '@mui/material/Box'
 import Dialog from '@mui/material/Dialog'
 import IconButton from '@mui/material/IconButton'
@@ -6,6 +6,7 @@ import CloseIcon from '@mui/icons-material/Close'
 import { PaperProps } from '@mui/material'
 
 import useBreakpoints from '~/hooks/use-breakpoints'
+import ConfirmDialog from '~/components/confirm-dialog/ConfirmDialog'
 import { styles } from '~/components/popup-dialog/PopupDialog.styles'
 
 interface PopupDialogProps {
@@ -24,35 +25,59 @@ const PopupDialog: FC<PopupDialogProps> = ({
   closeModalAfterDelay
 }) => {
   const { isMobile } = useBreakpoints()
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false)
+  const [isHaveUnsavedChanges, setIsHaveUnsavedChanges] = useState(false)
 
   const handleMouseOver = () => timerId && clearTimeout(timerId)
   const handleMouseLeave = () => timerId && closeModalAfterDelay()
 
   return (
-    <Dialog
-      PaperProps={paperProps}
-      data-testid='popup'
-      disableRestoreFocus
-      fullScreen={isMobile}
-      maxWidth='xl'
-      open
-    >
-      <Box
-        data-testid='popupContent'
-        onMouseLeave={handleMouseLeave}
-        onMouseOver={handleMouseOver}
-        sx={styles.box}
+    <>
+      <Dialog
+        PaperProps={paperProps}
+        data-testid='popup'
+        disableRestoreFocus
+        fullScreen={isMobile}
+        maxWidth='xl'
+        open
       >
-        <IconButton
-          data-testid='closeButton'
-          onClick={closeModal}
-          sx={styles.icon}
+        <Box
+          data-testid='popupContent'
+          onChangeCapture={(e) => {
+            const target = e.target as HTMLInputElement
+            if (target?.value !== '' || target?.checked === true) {
+              setIsHaveUnsavedChanges(true)
+            }
+          }}
+          onMouseLeave={handleMouseLeave}
+          onMouseOver={handleMouseOver}
+          sx={styles.box}
         >
-          <CloseIcon />
-        </IconButton>
-        <Box sx={styles.contentWraper}>{content}</Box>
-      </Box>
-    </Dialog>
+          <IconButton
+            data-testid='closeButton'
+            onClick={() => {
+              if (isHaveUnsavedChanges) {
+                setIsConfirmOpen(true)
+              } else {
+                closeModal()
+              }
+            }}
+            sx={styles.icon}
+          >
+            <CloseIcon />
+          </IconButton>
+          <Box sx={styles.contentWraper}>{content}</Box>
+        </Box>
+      </Dialog>
+
+      <ConfirmDialog
+        message='questions.unsavedChanges'
+        onConfirm={closeModal}
+        onDismiss={() => setIsConfirmOpen(false)}
+        open={isConfirmOpen}
+        title='titles.confirmTitle'
+      />
+    </>
   )
 }
 
