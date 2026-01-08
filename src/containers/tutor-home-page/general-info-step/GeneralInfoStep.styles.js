f44f780fc1953656b6673ea80bb717c4a82a3379
Gennadiy Naumovych <navmovn@gmail.com>
2026-01-08 04:22:32 +0200

    Added a style file to general info step

--- src/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles.js
+++ src/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles.js
@@ -3,9 +3,25 @@ import { fadeAnimation } from '~/styles/app-theme/custom-animations'
 export const styles = {
   container: {
     display: 'flex',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
     height: { sm: '485px' },
     paddingBottom: { xs: '30px', sm: '0' },
     ...fadeAnimation
+  },
+  contentBox: {
+    width: { xs: '100%', md: '45%' }
+  },
+  imgContainer: {
+    width: { xs: '0', md: '50%' },
+    height: { xs: '200px', sm: '485px' },
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  img: {
+    width: { xs: '100%', sm: '100%' },
+    height: { sm: 'auto', md: '485px' },
+    objectFit: { sm: 'cover' }
   }
 }
